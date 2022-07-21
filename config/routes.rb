12b971882439cfb709a9798a985d88f50aa62f6e
y.nguyen <y.nguyen@orientsoftware.com>
2022-07-21 09:45:28 +0700

    add about column to Authors table

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
   get '/authors/:author_id/articles', to: 'authors#show_all_article', as: 'show_all_articles'
+  get '/authors/:author_id/about', to: 'authors#about', as: 'about'
 
   resources :articles do
     resources :comments, :authors
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_20_070315) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_21_023307) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_20_070315) do
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "about"
   end
 
   create_table "comments", force: :cascade do |t|
