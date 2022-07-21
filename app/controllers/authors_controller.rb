b6b38a8b8df2c80dfabb31b2e6d98e0d97b24a10
y.nguyen <y.nguyen@orientsoftware.com>
2022-07-21 14:49:15 +0700

    add Author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,10 +1,27 @@
 class AuthorsController < ApplicationController
+    def index
+        @authors = Author.all
+    end
+
     def show_all_article
         @author = Author.find(params[:author_id])
-      end
+    end
 
     def show
         @author = Author.find(params[:id])
+    end
+
+    def new
+        @author = Author.new
+    end
+
+    def create
+        @author = Author.new(author_params)
+        if @author.save
+        redirect_to @author
+        else
+        render :new, status: :unprocessable_entity
+        end
     end
 
     def edit
@@ -14,7 +31,7 @@ class AuthorsController < ApplicationController
     def update
         @author = Author.find(params[:id])
     
-        if @author.update(athor_params)
+        if @author.update(author_params)
           redirect_to @author
         else
           render :edit, status: :unprocessable_entity
@@ -22,7 +39,7 @@ class AuthorsController < ApplicationController
       end
 
       private
-    def athor_params
+    def author_params
       params.require(:author).permit(:name, :age, :about)
     end
 end
